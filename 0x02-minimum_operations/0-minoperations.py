ab83722d29d904ab43e3ba475795a3e729a07a9a
AliyahTanzil <sesaymohamdaugustin@gmail.com>
2023-07-13 12:52:52 +0000

    Update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,23 @@
 #!/usr/bin/python3
 """
-Script that takes an integer n as the input and returns an integer as the output.
+Script that takes an integer n as the input and
+returns an integer as the output.
 """
 
 
 def minOperations(n):
-    """Calculates the minimum number of operations to get n H characters in a file.
+    """Calculates the minimum number of operations to get n
+    H characters in a file.
 
-    The only operations allowed are Copy All and Paste. The file initially contains one H character.
+    The only operations allowed are Copy All and Paste.
+    The file initially contains one H character.
 
     Args:
       n (int): The target number of H characters in the file.
 
     Returns:
-      int: The minimum number of operations, or 0 if n is not a positive integer.
+      int: The minimum number of operations,
+      or 0 if n is not a positive integer.
     """
     # If n is not a positive integer, return 0
     if n <= 0 or not isinstance(n, int):
